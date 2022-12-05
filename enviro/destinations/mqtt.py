1eb3059545f5abe94d7a900f032a8877652600b8
David Edwards <daveedwards1979@gmail.com>
2022-12-05 16:03:53 +0000

    Add support for SSL when using mqtt
    Add support for a local mqtt broke that uses ssl.  Allow user to add in their ca,crt file and if exists use SSL for mqtt uploads.

--- enviro/destinations/mqtt.py
+++ enviro/destinations/mqtt.py
@@ -12,10 +12,22 @@ def upload_reading(reading):
   username = config.mqtt_broker_username
   password = config.mqtt_broker_password
   nickname = reading["nickname"]
-
+  
   try:
     # attempt to publish reading
-    mqtt_client = MQTTClient(reading["uid"], server, user=username, password=password, keepalive=60)
+    
+    ##### SSL Change Start
+    # two options, with or without SSL
+    if config.mqtt_broker_ca_file:
+      f = open("ca.crt")
+      ssl_data = f.read()
+      f.close()
+      mqtt_client = MQTTClient(reading["uid"], server, user=username, password=password, keepalive=60,
+                               ssl=True, ssl_params={'cert': ssl_data})
+    else:
+    ##### SSL Change not set so do insecure connection
+      mqtt_client = MQTTClient(reading["uid"], server, user=username, password=password, keepalive=60)
+    # Now continue with connection and upload
     mqtt_client.connect()
     mqtt_client.publish(f"enviro/{nickname}", ujson.dumps(reading), retain=True)
     mqtt_client.disconnect()
