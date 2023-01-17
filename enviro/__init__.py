78f18b66c95baaec1602f157c67eb32c8e9a5591
Mike Cifelli <1836280-mike-cifelli@users.noreply.gitlab.com>
2023-01-17 12:50:35 -0500

    Fix import issue

--- enviro/__init__.py
+++ enviro/__init__.py
@@ -1,7 +1,6 @@
 # keep the power rail alive by holding VSYS_EN high as early as possible
 # ===========================================================================
 from enviro.constants import *
-from enviro.provisioning import write_config
 from machine import Pin
 hold_vsys_en_pin = Pin(HOLD_VSYS_EN_PIN, Pin.OUT, value=True)
 
